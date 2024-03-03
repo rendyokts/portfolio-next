c73bed8780d36421dccd6fee88a658e9beda5684
Rendi <rendyoktavian08@gmail.com>
2024-03-03 11:37:32 +0700

    Update Career

--- src/app/commons/constants/CareerLists.js
+++ src/app/commons/constants/CareerLists.js
@@ -1,15 +1,17 @@
 import { BiQuestionMark } from "react-icons/bi";
+import logoImage from '../public/zki.png';
 
 const ICON_SIZE = '30'
 export const CareerLists = [
   {
-    name: 'Coming Soon',
+    name: 'Zona Kreatif ID',
     link: '',
-    logo: <BiQuestionMark size={ICON_SIZE}/>,
-    location: 'Jakarta',
-    date: 'July 2023 - Present',
-    during: '~ 6 Months',
-    profession: 'Fullstack Developer',
+    // logo: <BiQuestionMark size={ICON_SIZE}/>,
+    logo: <img src={logoImage} alt={"Zona Kreatif ID" width={ICON_SIZE} height={ICON_SIZE} />,
+    location: 'Depok',
+    date: 'Maret 2023 - Present',
+    during: '~ 1 Year',
+    profession: 'Front End Developer',
   },
   {
     name: 'Coming Soon',
